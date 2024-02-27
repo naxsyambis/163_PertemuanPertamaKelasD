7f7f3fa58c2950b0f6144f3f9037761afe227588
naxsyambis <naxsyambis@gmail.com>
2024-02-27 11:19:09 +0000

    Add project files.

---
+++ PertemuanPertamaKelasD/PertemuanPertamaKelasD.cpp
@@ -0,0 +1,29 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{//begin
+
+	//numeric nAlas, nTinggi, nLuas
+	//display 'Masukan Alasnya= '
+	//accept nAlas
+	//display 'Masukan Tingginya= '
+	//accept nTinggi
+	//compute nLuas = 1/2 * nAlas * nTinggi
+	//display 'Luasnya adalah = ' + nLuas
+
+	int nAlas, nTinggi;
+	double nLuas;
+	cout << "Masukan Alasnya= ";
+	cin >> nAlas;
+	cout << "Masukan Tingginya= ";
+	cin >> nTinggi;
+	nLuas = 0.5 * (nAlas * nTinggi);
+	cout << "Luasnya adalah= " << nLuas << endl;
+
+	system("pause");
+
+}//end
